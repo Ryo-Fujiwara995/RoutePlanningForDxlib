8fba6fe3905b3d5c279066719bd2aceb0f73bc44
230660 <230660@jc-21.jp>
2025-02-04 10:52:11 +0900

    プロジェクト ファイルを追加します。

---
+++ Enemy.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "./Library/GameObject.h"
+#include "./globals.h"
+
+
+class Enemy :
+    public GameObject
+{
+    Point pos_;
+    bool isAlive_;
+    float speed_;
+public:
+    Enemy();
+    ~Enemy();
+
+    void Update() override;
+    void Draw() override;
+    bool CheckHit(const Rect& me, const Rect& other);
+};
+
